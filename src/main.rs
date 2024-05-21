66beac444d15b58bc5de7b172d1ae24f665019eb
hiten52 <hitensolanki010@gmail.com>
2024-05-21 17:17:53 +0530

    add query file

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
+use std::fs;
+
 mod database;
 
 fn main() {
-    let database_file_name = "database/movie_data.json";
-    let db = database::load_database(database_file_name).unwrap();
-
+    let database_file_path = "database/movie_data.json";
+    let db = database::load_database(database_file_path).unwrap();
+    
+    let query_file_path = "query";
+    let sql_query = fs::read_to_string(query_file_path).unwrap();
+   
     println!("{db:?}");
+    println!("{sql_query}");
 }
